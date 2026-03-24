a206d819384f238f02f91e7f9f9d376260eed73b
100591176 <100591176@alumnos.uc3m.es>
2026-03-24 20:04:54 +0100

    Added more tests that shouldve been in last test commit

--- G87.2026.T08.EG2-main/src/unittest/python/test_register_document.py
+++ G87.2026.T08.EG2-main/src/unittest/python/test_register_document.py
@@ -80,3 +80,77 @@ class TestRegisterDocument(TestCase):
             '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12-45.pdf"}',
             "JSON data has no valid values."
         )
+
+    def test_register_document_tc05_duplicate_root_node(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.xlsx"}'
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.xlsx"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc06_delete_first_bracket(self):
+        self._assert_exception_message(
+            '"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc07_duplicate_first_bracket(self):
+        self._assert_exception_message(
+            '{{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc09_duplicate_fields(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"'
+            '"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc10_delete_ending_bracket(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.xlsx"',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc11_duplicate_ending_bracket(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.xlsx"}}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc15_delete_comma(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7""FILENAME":"ABC12345.xlsx"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc16_duplicate_comma(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7",,"FILENAME":"ABC12345.xlsx"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc22_delete_colon_field1(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID""84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc23_duplicate_colon_field1(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID"::"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc29_delete_colon_field2(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID":"84a2b5abfa27576259e41a033d07cee7","FILENAME""ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
+
+    def test_register_document_tc37_delete_label_field1_end_quote(self):
+        self._assert_exception_message(
+            '{"PROJECT_ID:"84a2b5abfa27576259e41a033d07cee7","FILENAME":"ABC12345.pdf"}',
+            "The file is not JSON formatted."
+        )
